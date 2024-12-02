d7d6fa60261c8d40be4c48e161745b23d81e0670
psl <petr.sladek@webnode.com>
2024-12-02 08:35:43 +0100

    1. day

---
+++ day1/process.php
@@ -0,0 +1,42 @@
+<?php
+
+$file = new SplFileObject("input.txt");
+while($line = $file->fgets())
+{
+	$values = explode("  ", trim($line));
+
+	$list1[] = $values[0];
+	$list2[] = $values[1];
+}
+
+
+sort($list1);
+sort($list2);
+
+$sum = 0;
+foreach ($list1 as $key => $value)
+{
+	$sum += abs($list1[$key] - $list2[$key]);
+}
+
+
+echo $sum . "\n";
+
+
+
+$sum = 0;
+foreach ($list1 as $value)
+{
+	$count = 0;
+	foreach ($list2 as $value2)
+	{
+		if ($value2 == $value)
+		{
+			$count++;
+		}
+	}
+
+	$sum += ($value * $count);
+}
+
+echo $sum . "\n";
